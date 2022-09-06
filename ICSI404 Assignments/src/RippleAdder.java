cf3a2d6fb842916134db2bb9444f2fe5f184ab80
ngbon <ngbon@169.226.217.161>
2022-09-06 14:43:08 -0400

    Add RippleAdder

---
+++ ICSI404 Assignments/src/RippleAdder.java
@@ -0,0 +1,14 @@
+public class RippleAdder {
+
+	public static Longword add(Longword a, Longword b) {
+		Longword result = new Longword();
+		
+		return result;
+	}
+	
+	public static Longword subtract(Longword a, Longword b) {
+		Longword result = new Longword();
+		
+		return result;
+	}
+}
