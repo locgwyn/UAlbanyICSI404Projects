c91804b10fe1ed0f0f87146eeaaa2199f0051197
Nico <Nico@169.226.75.86>
2022-09-08 14:50:50 -0400

    RippleAdder add method v1

--- ICSI404 Assignments/src/RippleAdder.java
+++ ICSI404 Assignments/src/RippleAdder.java
@@ -2,15 +2,38 @@ public class RippleAdder {
 
 	public static Longword add(Longword a, Longword b) {
 		Longword result = new Longword();
-		
+		boolean carry = false;
+		for (int x = 31; x >= 0; x--) {
+			if (carry == true) {
+				if (a.getBit(x).and(b.getBit(x)).getValue() == true) {
+					result.setBit(x, new Bit(true));
+					carry = true;
+				} else if (a.getBit(x).or(b.getBit(x)).getValue() == true) {
+					result.setBit(x, new Bit(false));
+					carry = true;
+				} else {
+					result.setBit(x, new Bit(true));
+					carry = false;
+				}
+			} else {
+				if (a.getBit(x).and(b.getBit(x)).getValue() == true) {
+					result.setBit(x, new Bit(false));
+					carry = true;
+				} else if (a.getBit(x).or(b.getBit(x)).getValue() == true) {
+					result.setBit(x, new Bit(true));
+					carry = false;
+				} else {
+					result.setBit(x, new Bit(false));
+					carry = false;
+				}
+			}
+		}
 		return result;
 	}
-	
+
 	public static Longword subtract(Longword a, Longword b) {
 		Longword result = new Longword();
-		
+
 		return result;
 	}
-	
-	// TEST FOR GITHUB
 }
