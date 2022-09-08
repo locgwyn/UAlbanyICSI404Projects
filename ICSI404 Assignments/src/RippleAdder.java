fb1e712ab99591f23b328102cda3deb7b5b0c16d
Nico <Nico@169.226.75.86>
2022-09-08 18:21:21 -0400

    Removed code from add

--- ICSI404 Assignments/src/RippleAdder.java
+++ ICSI404 Assignments/src/RippleAdder.java
@@ -1,35 +1,14 @@
 public class RippleAdder {
 
 	/*
-	 * USE XOR FOR THE RIPPLE ADDER (tt = f, ff = f, tf = t, ft = t)
+	 * USE XOR FOR THE RIPPLE ADDER (tt = f, ff = f, tf = t, ft = t) Should only
+	 * really use 1 if statement
 	 */
 	public static Longword add(Longword a, Longword b) {
 		Longword result = new Longword();
 		boolean carry = false;
 		for (int x = 31; x >= 0; x--) {
-			if (carry == true) {
-				if (a.getBit(x).and(b.getBit(x)).getValue() == true) {
-					result.setBit(x, new Bit(true));
-					carry = true;
-				} else if (a.getBit(x).or(b.getBit(x)).getValue() == true) {
-					result.setBit(x, new Bit(false));
-					carry = true;
-				} else {
-					result.setBit(x, new Bit(true));
-					carry = false;
-				}
-			} else {
-				if (a.getBit(x).and(b.getBit(x)).getValue() == true) {
-					result.setBit(x, new Bit(false));
-					carry = true;
-				} else if (a.getBit(x).or(b.getBit(x)).getValue() == true) {
-					result.setBit(x, new Bit(true));
-					carry = false;
-				} else {
-					result.setBit(x, new Bit(false));
-					carry = false;
-				}
-			}
+
 		}
 		return result;
 	}
