078f597faa9e7262371692abe8430903b6363668
Nico <Nico@LAPTOP-RNO5M0G7.fios-router.home>
2022-09-10 12:10:22 -0400

    Finished RippleAdder sub method, both add and sub need testing

--- ICSI404 Assignments/src/RippleAdder.java
+++ ICSI404 Assignments/src/RippleAdder.java
@@ -18,9 +18,14 @@ public class RippleAdder {
 
 	public static Longword subtract(Longword a, Longword b) {
 		Longword result = new Longword();
+		// Get two's complement of b, since subtracting is just adding the negation of a number
+		Longword bComplement = b.getTwosComplement();
 		for (int x = 31; x >= 0; x--) {
 			Bit carry = new Bit(false);
 			Bit sum = new Bit(false);
+			sum = a.getBit(x).xor(bComplement.getBit(x)).xor(carry);
+			carry = a.getBit(x).and(bComplement.getBit(x)).or((a.getBit(x).xor(bComplement.getBit(x))).and(carry));
+			result.setBit(x, sum);
 		}
 		return result;
 	}
