287168c3d420ce2060ce37af69a0274662f1921b
Nico <Nico@LAPTOP-RNO5M0G7.fios-router.home>
2022-09-11 10:31:15 -0400

    Modified code in RippleAdder

--- ICSI404 Assignments/src/RippleAdder.java
+++ ICSI404 Assignments/src/RippleAdder.java
@@ -1,10 +1,9 @@
 public class RippleAdder {
 	public static Longword add(Longword a, Longword b) {
 		Longword result = new Longword();
+		Bit carry = new Bit(false);
 		for (int x = 31; x >= 0; x--) {
-			Bit carry = new Bit(false);
-			Bit sum = new Bit(false);
-			sum = a.getBit(x).xor(b.getBit(x)).xor(carry);
+			Bit sum = a.getBit(x).xor(b.getBit(x)).xor(carry);
 			carry = a.getBit(x).and(b.getBit(x)).or((a.getBit(x).xor(b.getBit(x))).and(carry));
 			result.setBit(x, sum);
 		}
@@ -18,8 +17,7 @@ public class RippleAdder {
 		Longword bComplement = b.getTwosComplement();
 		for (int x = 31; x >= 0; x--) {
 			Bit carry = new Bit(false);
-			Bit sum = new Bit(false);
-			sum = a.getBit(x).xor(bComplement.getBit(x)).xor(carry);
+			Bit sum = a.getBit(x).xor(bComplement.getBit(x)).xor(carry);
 			carry = a.getBit(x).and(bComplement.getBit(x)).or((a.getBit(x).xor(bComplement.getBit(x))).and(carry));
 			result.setBit(x, sum);
 		}
