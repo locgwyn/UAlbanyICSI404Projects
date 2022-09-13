abc76f3339d189e0964802b63dc4bbc18a3c387c
Nico <Nico@169.226.227.64>
2022-09-13 16:00:14 -0400

    Added Multiplier and ALU class

---
+++ ICSI404 Assignments/src/ALU.java
@@ -0,0 +1,8 @@
+
+public class ALU {
+	public static Longword doOp(Bit[] operation, Longword a, Longword b) {
+		Longword result = new Longword();
+		
+		return result;
+	}
+}
---
+++ ICSI404 Assignments/src/Multiplier.java
@@ -0,0 +1,8 @@
+
+public class Multiplier {
+	public static Longword multiply(Longword a, Longword b) {
+		Longword result = new Longword();
+		
+		return result;
+	}
+}
