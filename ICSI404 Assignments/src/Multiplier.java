9c6524af07e31455d899f4d11f769e3fe5d92224
Nico <Nico@169.226.227.64>
2022-09-13 16:59:57 -0400

    Finished prototype of Multiplier

--- ICSI404 Assignments/src/Multiplier.java
+++ ICSI404 Assignments/src/Multiplier.java
@@ -2,7 +2,15 @@
 public class Multiplier {
 	public static Longword multiply(Longword a, Longword b) {
 		Longword result = new Longword();
-		
+		int shiftAmt = 0;
+
+		// Iterate through Longword b, if Bit at index is true, left shift by 31 - index
+		for (int x = 31; x >= 0; x--) {
+			if (b.getBit(x).getValue() == true) {
+				shiftAmt = 31 - x;
+				result.copy(RippleAdder.add(result, a.leftShift(shiftAmt)));
+			}
+		}
 		return result;
 	}
 }
