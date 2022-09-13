a4c91d484a70d3ba6bf8b282d736687358e9047d
Nico <Nico@169.226.227.64>
2022-09-13 18:17:55 -0400

    Adjusted comments in Multiplier

--- ICSI404 Assignments/src/Multiplier.java
+++ ICSI404 Assignments/src/Multiplier.java
@@ -4,7 +4,8 @@ public class Multiplier {
 		Longword result = new Longword();
 		int shiftAmt = 0;
 
-		// Iterate through Longword b, if Bit at index is true, left shift by 31 - index
+		// Iterate through Longword b, if Bit at index is true, 
+		// left shift a by 31 - index and then add result
 		for (int x = 31; x >= 0; x--) {
 			if (b.getBit(x).getValue() == true) {
 				shiftAmt = 31 - x;
