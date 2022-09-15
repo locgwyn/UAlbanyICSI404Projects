d3039f9f77702e2181006aa9b6cc4a327f5b887b
Nico <Nico@169.226.216.84>
2022-09-15 18:30:55 -0400

    Added if blocks and results to all but left/right shift

--- ICSI404 Assignments/src/ALU.java
+++ ICSI404 Assignments/src/ALU.java
@@ -2,7 +2,53 @@
 public class ALU {
 	public static Longword doOp(Bit[] operation, Longword a, Longword b) {
 		Longword result = new Longword();
-		
+
+		// AND
+		if (operation[0].getValue() == true && operation[1].getValue() == false && operation[2].getValue() == false
+				&& operation[3].getValue() == false) {
+			result.copy(a.and(b));
+		}
+		// OR
+		if (operation[0].getValue() == true && operation[1].getValue() == false && operation[2].getValue() == false
+				&& operation[3].getValue() == true) {
+			result.copy(a.or(b));
+		}
+		// XOR
+		if (operation[0].getValue() == true && operation[1].getValue() == false && operation[2].getValue() == true
+				&& operation[3].getValue() == false) {
+			result.copy(a.xor(b));
+		}
+		// NOT (a)
+		if (operation[0].getValue() == true && operation[1].getValue() == false && operation[2].getValue() == true
+				&& operation[3].getValue() == true) {
+			result.copy(a.not());
+		}
+		// LEFT SHIFT
+		if (operation[0].getValue() == true && operation[1].getValue() == true && operation[2].getValue() == false
+				&& operation[3].getValue() == false) {
+
+		}
+		// RIGHT SHIFT
+		if (operation[0].getValue() == true && operation[1].getValue() == true && operation[2].getValue() == false
+				&& operation[3].getValue() == true) {
+
+		}
+		// ADD
+		if (operation[0].getValue() == true && operation[1].getValue() == true && operation[2].getValue() == true
+				&& operation[3].getValue() == false) {
+			result.copy(RippleAdder.add(a, b));
+		}
+		// SUBTRACT
+		if (operation[0].getValue() == true && operation[1].getValue() == true && operation[2].getValue() == true
+				&& operation[3].getValue() == true) {
+			result.copy(RippleAdder.subtract(a, b));
+		}
+		// MULTIPLY
+		if (operation[0].getValue() == false && operation[1].getValue() == true && operation[2].getValue() == true
+				&& operation[3].getValue() == true) {
+			result.copy(Multiplier.multiply(a, b));
+		}
+
 		return result;
 	}
 }
