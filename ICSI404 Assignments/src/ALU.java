48fc7be26e041e738207da2e3eb465f335a38872
Nico <Nico@DESKTOP-1TQIHUB.fios-router.home>
2022-09-18 09:28:57 -0400

    Finished ALU left/right shift
    Started ALU_test

--- ICSI404 Assignments/src/ALU.java
+++ ICSI404 Assignments/src/ALU.java
@@ -8,41 +8,58 @@ public class ALU {
 				&& operation[3].getValue() == false) {
 			result.copy(a.and(b));
 		}
+
 		// OR
 		if (operation[0].getValue() == true && operation[1].getValue() == false && operation[2].getValue() == false
 				&& operation[3].getValue() == true) {
 			result.copy(a.or(b));
 		}
+
 		// XOR
 		if (operation[0].getValue() == true && operation[1].getValue() == false && operation[2].getValue() == true
 				&& operation[3].getValue() == false) {
 			result.copy(a.xor(b));
 		}
+
 		// NOT (a)
 		if (operation[0].getValue() == true && operation[1].getValue() == false && operation[2].getValue() == true
 				&& operation[3].getValue() == true) {
 			result.copy(a.not());
 		}
+
 		// LEFT SHIFT
 		if (operation[0].getValue() == true && operation[1].getValue() == true && operation[2].getValue() == false
 				&& operation[3].getValue() == false) {
+			Longword temp = a.leftShift(b.getSigned());
+			// ignores all but the first 5 bits
+			for (int x = 31; x > 26; x--) {
+				result.setBit(x, temp.getBit(x));
+			}
 
 		}
+
 		// RIGHT SHIFT
 		if (operation[0].getValue() == true && operation[1].getValue() == true && operation[2].getValue() == false
 				&& operation[3].getValue() == true) {
-
+			Longword temp = a.rightShift(b.getSigned());
+			// ignores all but the first 5 bits
+			for (int x = 31; x > 26; x--) {
+				result.setBit(x, temp.getBit(x));
+			}
 		}
+
 		// ADD
 		if (operation[0].getValue() == true && operation[1].getValue() == true && operation[2].getValue() == true
 				&& operation[3].getValue() == false) {
 			result.copy(RippleAdder.add(a, b));
 		}
+
 		// SUBTRACT
 		if (operation[0].getValue() == true && operation[1].getValue() == true && operation[2].getValue() == true
 				&& operation[3].getValue() == true) {
 			result.copy(RippleAdder.subtract(a, b));
 		}
+
 		// MULTIPLY
 		if (operation[0].getValue() == false && operation[1].getValue() == true && operation[2].getValue() == true
 				&& operation[3].getValue() == true) {
--- ICSI404 Assignments/src/ALU_test.java
+++ ICSI404 Assignments/src/ALU_test.java
@@ -1,4 +1,21 @@
-
 public class ALU_test {
+	public static void main(String args[]) {
+		Bit_test.runTests();
+		System.out.println();
+		Longword_test.runTests();
+		System.out.println();
+		RippleAdder_test.runTests();
+		System.out.println();
+		Multiplier_test.runTests();
+		System.out.println();
+		runTests();
+	}
 
+	public static void runTests() {
+		testALU();
+	}
+
+	public static void testALU() {
+		System.out.println("PLACEHOLDER");
+	}
 }
