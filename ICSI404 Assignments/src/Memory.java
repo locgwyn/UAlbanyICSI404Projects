816b4a32d9ffe37125c80f79a979c30e4140660c
Nico <Nico@DESKTOP-1TQIHUB.fios-router.home>
2022-09-20 10:26:11 -0400

    Added memory class

---
+++ ICSI404 Assignments/src/Memory.java
@@ -0,0 +1,15 @@
+
+public class Memory {
+
+	private Bit[] memory = new Bit[8192];
+	
+	public Longword read(Longword address) {
+		Longword readResult = new Longword();
+		
+		return readResult;
+	}
+	
+	public void write(Longword address, Longword value) {
+		
+	}
+}
