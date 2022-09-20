a5ecfe3a55ba43de9afed6786e44e2ff7c1301cb
Nico <Nico@169.226.231.106>
2022-09-20 18:01:36 -0400

    Added run, fetch, decode, execute, store methods

--- ICSI404 Assignments/src/Computer.java
+++ ICSI404 Assignments/src/Computer.java
@@ -1,4 +1,30 @@
 
 public class Computer {
 
+	private Bit running = new Bit(false);
+	
+	public void run() {
+		while (running.getValue() == true) {
+			fetch();
+			decode();
+			execute();
+			store();
+		}
+	}
+	
+	public void fetch() {
+		
+	}
+	
+	public void decode() {
+		
+	}
+	
+	public void execute() {
+		
+	}
+	
+	public void store() {
+		
+	}
 }
