3f261573390b407aca893126034273193c844dc5
Nico <Nico@DESKTOP-1TQIHUB.fios-router.home>
2022-09-25 18:21:07 -0400

    Finished Memory and Memory_test

--- ICSI404 Assignments/src/Memory.java
+++ ICSI404 Assignments/src/Memory.java
@@ -10,16 +10,22 @@ public class Memory {
 		}
 	}
 
+	// address = 0, read/write from bit 0 to 7
+	// address = 1, read.write from bit 8 to 15
+	// ... and so on
+
 	public Longword read(Longword address) {
 		Longword readResult = new Longword();
+		
 		// Address is by byte for a total of 1024 possible address
 		int memoryAddress = (address.getSigned() * 8);
-		
+
 		// Used to decrement the index of the longword to be returned
 		int resultIndex = 31;
-		
-		// Reads the bits in memory from right to left, only reads 8 bits since reading by byte
-		for (int x = memoryAddress + 7 ; x >= memoryAddress; x--) {
+
+		// Reads the bits in memory from right to left, only reads 8 bits since reading
+		// by byte
+		for (int x = memoryAddress + 7; x >= memoryAddress; x--) {
 			Bit tempBit = new Bit(memory[x].getValue());
 			readResult.setBit(resultIndex, tempBit);
 			resultIndex--;
@@ -27,15 +33,17 @@ public class Memory {
 		return readResult;
 	}
 
-	// address = 0, read from bit 0 to 7 and copy to another longword
-	// address = 1, read from bit 8 to 15 and copy to another longword
-	// address = 2, read from bit 16 to 23 and copy to another longword
-	// address = 3, read from bit 24 to 31 and copy to another longword
-
 	public void write(Longword address, Longword value) {
+		
+		// total 1024 addresses
 		int memoryAddress = (address.getSigned() * 8);
+		
+		// Used to decrement the index of the longword to be written to memory
 		int writeIndex = 31;
-		for (int x = memoryAddress +7; x >= memoryAddress; x--) {
+
+		// Writes the bits in memory from right to left, writing only 8 bits since
+		// writing by byte
+		for (int x = memoryAddress + 7; x >= memoryAddress; x--) {
 			memory[x] = value.getBit(writeIndex);
 			writeIndex--;
 		}
--- ICSI404 Assignments/src/Memory_test.java
+++ ICSI404 Assignments/src/Memory_test.java
@@ -1,3 +1,77 @@
 public class Memory_test {
+	public static void main(String args[]) {
+		Bit_test.runTests();
+		System.out.println();
+		Longword_test.runTests();
+		System.out.println();
+		RippleAdder_test.runTests();
+		System.out.println();
+		Multiplier_test.runTests();
+		System.out.println();
+		ALU_test.runTests();
+		System.out.println();
+		runTests();
+	}
 
+	public static void runTests() {
+		testMemory();
+	}
+
+	public static void testMemory() {
+		Memory test1 = new Memory();
+		Longword testData1 = new Longword();
+		Longword testAddress1 = new Longword();
+		String data1 = "fffffffffffffffffffffffftftfftft";
+		testData1.set(165); // 10100101 or fffffffffffffffffffffffftftfftft
+		testAddress1.set(1); // Address 1
+
+		test1.write(testAddress1, testData1);
+		if (test1.read(testAddress1).toString().equals(data1)) {
+			System.out.println("Write binary 165 (tftfftft) to memory address 1, Expected read " + data1 + " : PASS");
+		} else {
+			System.out.println("Write binary 165 (tftfftft) to memory address 1, Expected read " + data1 + " : FAIL");
+		}
+
+		Memory test2 = new Memory();
+		Longword testData2 = new Longword();
+		Longword testAddress2 = new Longword();
+		String data2 = "fffffffffffffffffffffffftffftttf";
+		testData2.set(142); // 10001110 or fffffffffffffffffffffffftffftttf
+		testAddress2.set(1023);
+
+		test2.write(testAddress2, testData2);
+		if (test2.read(testAddress2).toString().equals(data2)) {
+			System.out.println("Write binary 142 (tffftttf) to memory address 1023, Expected read " + data2 + " : PASS");
+		} else {
+			System.out.println("Write binary 142 (tffftttf) to memory address 1023, Expected read " + data2 + " : FAIL");
+		}
+
+		Memory test3 = new Memory();
+		Longword testData3 = new Longword();
+		Longword testAddress3 = new Longword();
+		String data3 = "ffffffffffffffffffffffffftftfttt";
+		testData3.set(87); // 01010111 or ffffffffffffffffffffffffftftfttt
+		testAddress3.set(591);
+
+		test3.write(testAddress3, testData3);
+		if (test3.read(testAddress3).toString().equals(data3)) {
+			System.out.println("Write binary 87 (ftftfttt) to memory address 591, Expected read " + data3 + " : PASS");
+		} else {
+			System.out.println("Write binary 87 (ftftfttt) to memory address 591, Expected read " + data3 + " : FAIL");
+		}
+
+		Memory test4 = new Memory();
+		Longword testData4 = new Longword();
+		Longword testAddress4 = new Longword();
+		String data4 = "fffffffffffffffffffffffffffttfft";
+		testData4.set(25); // 00011001 or fffffffffffffffffffffffffffttfft
+		testAddress4.set(0);
+
+		test4.write(testAddress4, testData4);
+		if (test4.read(testAddress4).toString().equals(data4)) {
+			System.out.println("Write binary 25 (fffttfft) to memory address 0, Expected read " + data4 + " : PASS");
+		} else {
+			System.out.println("Write binary 25 (fffttfft) to memory address 0, Expected read " + data4 + " : FAIL");
+		}
+	}
 }
