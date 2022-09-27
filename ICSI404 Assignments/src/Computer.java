9f1c4791d8110bbbc95add4d36af9bb06412f31a
Nico <Nico@169.226.216.122>
2022-09-27 18:49:10 -0400

    Added PC, currentInstruction to Computer
    Changed logic in Memory to go by Longword instead of by byte

--- ICSI404 Assignments/src/Computer.java
+++ ICSI404 Assignments/src/Computer.java
@@ -2,8 +2,11 @@ public class Computer {
 
 	private Bit running = new Bit(false);
 	private Memory computerMemory = new Memory();
-	
+	private Longword PC = new Longword();
+	private Longword currentInstruction = new Longword();
+
 	public void run() {
+		PC.set(0);
 		while (running.getValue() == true) {
 			fetch();
 			decode();
@@ -11,20 +14,20 @@ public class Computer {
 			store();
 		}
 	}
-	
+
 	public void fetch() {
-		
+		int currentInstructionIndex = 31;
 	}
-	
+
 	public void decode() {
-		
+
 	}
-	
+
 	public void execute() {
-		
+
 	}
-	
+
 	public void store() {
-		
+
 	}
 }
--- ICSI404 Assignments/src/Memory.java
+++ ICSI404 Assignments/src/Memory.java
@@ -16,36 +16,77 @@ public class Memory {
 
 	public Longword read(Longword address) {
 		Longword readResult = new Longword();
-		
+
 		// Address is by byte for a total of 1024 possible address
-		int memoryAddress = (address.getSigned() * 8);
+		int memoryAddress = address.getSigned();
 
 		// Used to decrement the index of the longword to be returned
 		int resultIndex = 31;
 
-		// Reads the bits in memory from right to left, only reads 8 bits since reading
-		// by byte
-		for (int x = memoryAddress + 7; x >= memoryAddress; x--) {
-			Bit tempBit = new Bit(memory[x].getValue());
-			readResult.setBit(resultIndex, tempBit);
-			resultIndex--;
+		// Reads the bits in memory from right to left, reads up to 32 bits since
+		// reading by longword except in last 3 bytes, if statements added for these
+		// cases
+
+		if (memoryAddress == 1021) { // Reads the last 3 bytes of memory
+			for (int x = (memoryAddress * 8) + 23; x >= memoryAddress * 8; x--) {
+				Bit tempBit = new Bit(memory[x].getValue());
+				readResult.setBit(resultIndex, tempBit);
+				resultIndex--;
+			}
+		} else if (memoryAddress == 1022) { // Reads the last 2 bytes of memory
+			for (int x = (memoryAddress * 8) + 15; x >= memoryAddress * 8; x--) {
+				Bit tempBit = new Bit(memory[x].getValue());
+				readResult.setBit(resultIndex, tempBit);
+				resultIndex--;
+			}
+		} else if (memoryAddress == 1023) { // Reads the last byte of memory
+			for (int x = (memoryAddress * 8) + 7; x >= memoryAddress * 8; x--) {
+				Bit tempBit = new Bit(memory[x].getValue());
+				readResult.setBit(resultIndex, tempBit);
+				resultIndex--;
+			}
+		} else { // Addresses 0 - 1020
+			for (int x = (memoryAddress * 8) + 31; x >= memoryAddress * 8; x--) {
+				Bit tempBit = new Bit(memory[x].getValue());
+				readResult.setBit(resultIndex, tempBit);
+				resultIndex--;
+			}
 		}
 		return readResult;
 	}
 
 	public void write(Longword address, Longword value) {
-		
+
 		// total 1024 addresses
-		int memoryAddress = (address.getSigned() * 8);
-		
+		int memoryAddress = address.getSigned();
+
 		// Used to decrement the index of the longword to be written to memory
 		int writeIndex = 31;
 
-		// Writes the bits in memory from right to left, writing only 8 bits since
-		// writing by byte
-		for (int x = memoryAddress + 7; x >= memoryAddress; x--) {
-			memory[x] = value.getBit(writeIndex);
-			writeIndex--;
+		// Writes the bits in memory from right to left, writes up to 32 bits since
+		// writing by Longword except in last 3 bytes, if statements added for these
+		// cases
+
+		if (memoryAddress == 1021) { // Writes to last 3 bytes of memory
+			for (int x = (memoryAddress * 8) + 23; x >= memoryAddress * 8; x--) {
+				memory[x] = value.getBit(writeIndex);
+				writeIndex--;
+			}
+		} else if (memoryAddress == 1022) { // Writes to last 2 bytes of memory
+			for (int x = (memoryAddress * 8) + 15; x >= memoryAddress * 8; x--) {
+				memory[x] = value.getBit(writeIndex);
+				writeIndex--;
+			}
+		} else if (memoryAddress == 1023) { // Writes to last byte of memory
+			for (int x = (memoryAddress * 8) + 7; x >= memoryAddress * 8; x--) {
+				memory[x] = value.getBit(writeIndex);
+				writeIndex--;
+			}
+		} else { // Addresses 0 - 1020
+			for (int x = (memoryAddress * 8) + 31; x >= memoryAddress * 8; x--) {
+				memory[x] = value.getBit(writeIndex);
+				writeIndex--;
+			}
 		}
 	}
 }
