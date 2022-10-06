08b78135beabb8761e338d401c3ff2aac436aa39
Nico <Nico@169.226.229.208>
2022-10-06 19:00:08 -0400

    Added initial logic for getting the two operands

--- ICSI404 Assignments/src/Computer.java
+++ ICSI404 Assignments/src/Computer.java
@@ -29,10 +29,12 @@ public class Computer {
 
 	public void decode() {
 		
-		// Determine op1, shift and then AND
+		// Determine op1, shift to get nibble 2 and then AND with 16 bit instruction
+		op1 = currentInstruction.rightShift(24).leftShift(28).rightShift(28).and(currentInstruction.rightShift(16));
+		// Determine op2, shift to get nibble 3 and then AND with 16 bit instruction
+		op2 = currentInstruction.rightShift(20).leftShift(24).rightShift(24).and(currentInstruction.rightShift(16));
 		
-		// Determine op2, shift and then AND
-		
+		// Both result in the index of the register to be copied from, need the actual value from the registers
 	}
 
 	public void execute() {
