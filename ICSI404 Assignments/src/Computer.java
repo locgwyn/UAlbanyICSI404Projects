2698a9183f2bba855e8d44bb676de4e72c5d51a1
Nico <Nico@DESKTOP-1TQIHUB.fios-router.home>
2022-10-09 16:37:20 -0400

    Cleaned up code in decode, execute, and store

--- ICSI404 Assignments/src/Computer.java
+++ ICSI404 Assignments/src/Computer.java
@@ -7,7 +7,6 @@ public class Computer {
 	private Longword[] registers = new Longword[16];
 	private Longword op1 = new Longword();
 	private Longword op2 = new Longword();
-	private Longword opCode = new Longword();
 	private Longword result = new Longword();
 
 	public void run() {
@@ -21,27 +20,41 @@ public class Computer {
 	}
 
 	public void fetch() {
-		currentInstruction = computerMemory.read(PC);
+		currentInstruction.copy(computerMemory.read(PC));
+		// Increment PC by 2
 		Longword increment = new Longword();
 		increment.set(2);
 		RippleAdder.add(PC, increment);
 	}
 
 	public void decode() {
+		Longword mask = new Longword();
+		mask.set(15);
+		// Determine op1 register, shift to get nibble 2 and then AND with mask of tttt (1111)
+		int op1Index = currentInstruction.rightShift(24).leftShift(28).rightShift(28).and(mask).getSigned();
+		op1.copy(registers[op1Index]);
+		// Determine op2 register, shift to get nibble 3 and then AND with mask of tttt (1111)
+		int op2Index = currentInstruction.rightShift(20).leftShift(28).rightShift(28).and(mask).getSigned();
+		op2.copy(registers[op2Index]);
 		
-		// Determine op1, shift to get nibble 2 and then AND with 16 bit instruction
-		op1 = currentInstruction.rightShift(24).leftShift(28).rightShift(28).and(currentInstruction.rightShift(16));
-		// Determine op2, shift to get nibble 3 and then AND with 16 bit instruction
-		op2 = currentInstruction.rightShift(20).leftShift(24).rightShift(24).and(currentInstruction.rightShift(16));
-		
-		// Both result in the index of the register to be copied from, need the actual value from the registers
 	}
 
 	public void execute() {
+		Bit[] opCode = new Bit[4]; // Holds the opCode from the instruction
+		
+		// Retrieve the opCode from the current instruction, get nibble 1
+		for (int x = 0; x < 4; x++) {
+			opCode[x].setBit(currentInstruction.getBit(x).getValue());
+		}
 
+		result.copy(ALU.doOp(opCode, op1, op2));
 	}
 
 	public void store() {
-
+		Longword mask = new Longword();
+		mask.set(15);
+		// Determine result register, shift to get nibble 4, and then AND with mask
+		int resultIndex = currentInstruction.rightShift(16).leftShift(28).rightShift(28).and(mask).getSigned();
+		registers[resultIndex].copy(result);
 	}
 }
