1de449f8037692d0cdcc19602c24d501ccb1bc8e
Nico <Nico@169.254.165.236>
2022-10-18 14:00:32 -0400

    Started implementation of move function

--- ICSI404 Assignments/src/Computer.java
+++ ICSI404 Assignments/src/Computer.java
@@ -1,13 +1,15 @@
 public class Computer {
 
-	private Bit running = new Bit(false);
+	private Bit running = new Bit(true);
 	private Memory computerMemory = new Memory();
 	private Longword PC = new Longword();
 	private Longword currentInstruction = new Longword();
-	
+
 	private Longword[] registers = new Longword[16];
-	private Longword op1 = new Longword();
-	private Longword op2 = new Longword();
+	private Bit[] opCode = new Bit[4]; // Holds the opCode from the instruction
+	private Longword op1 = new Longword(); // operand 1
+	private Longword op2 = new Longword(); // operand 2
+	private int moveVal;
 	private Longword result = new Longword();
 
 	public void run() {
@@ -31,24 +33,42 @@ public class Computer {
 	public void decode() {
 		Longword mask = new Longword();
 		mask.set(15);
-		// Determine op1 register, shift to get nibble 2 and then AND with mask of tttt (1111)
-		int op1Index = currentInstruction.rightShift(24).leftShift(28).rightShift(28).and(mask).getSigned();
-		op1.copy(registers[op1Index]);
-		// Determine op2 register, shift to get nibble 3 and then AND with mask of tttt (1111)
-		int op2Index = currentInstruction.rightShift(20).leftShift(28).rightShift(28).and(mask).getSigned();
-		op2.copy(registers[op2Index]);
-		
-	}
 
-	public void execute() {
-		Bit[] opCode = new Bit[4]; // Holds the opCode from the instruction
-		
 		// Retrieve the opCode from the current instruction, get nibble 1
 		for (int x = 0; x < 4; x++) {
 			opCode[x].setBit(currentInstruction.getBit(x).getValue());
 		}
 
-		result.copy(ALU.doOp(opCode, op1, op2));
+		// MOVE instruction
+		if (opCode[0].getValue() == false && opCode[1].getValue() == false && opCode[2].getValue() == false
+				&& opCode[3].getValue() == true) {
+			// Determine register to move value to (nibble 2)
+			int op1Index = currentInstruction.rightShift(24).leftShift(28).rightShift(28).and(mask).getSigned();
+			op1.copy(registers[op1Index]);
+			
+			// Determine the value to move into the register
+			
+		} else {
+			// Determine op1 register, shift to get nibble 2 and then AND with mask of tttt
+			// (1111)
+			int op1Index = currentInstruction.rightShift(24).leftShift(28).rightShift(28).and(mask).getSigned();
+			op1.copy(registers[op1Index]);
+
+			// Determine op2 register, shift to get nibble 3 and then AND with mask of tttt
+			// (1111)
+			int op2Index = currentInstruction.rightShift(20).leftShift(28).rightShift(28).and(mask).getSigned();
+			op2.copy(registers[op2Index]);
+
+		}
+	}
+
+	public void execute() {
+		if (opCode[0].getValue() == false && opCode[1].getValue() == false && opCode[2].getValue() == false
+				&& opCode[3].getValue() == false) { // HALT instruction
+			running.setBit(false);
+		} else { // Otherwise regular op instruction
+			result.copy(ALU.doOp(opCode, op1, op2));
+		}
 	}
 
 	public void store() {
