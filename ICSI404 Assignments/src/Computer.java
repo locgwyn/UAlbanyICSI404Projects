a289abb5bf351420042d36039bb6e30018f73812
Nico <Nico@DESKTOP-1TQIHUB.fios-router.home>
2022-10-18 20:25:49 -0400

    Implemented Move instruction

--- ICSI404 Assignments/src/Computer.java
+++ ICSI404 Assignments/src/Computer.java
@@ -6,11 +6,20 @@ public class Computer {
 	private Longword currentInstruction = new Longword();
 
 	private Longword[] registers = new Longword[16];
+
 	private Bit[] opCode = new Bit[4]; // Holds the opCode from the instruction
+
+	// Regular Instructions
 	private Longword op1 = new Longword(); // operand 1
 	private Longword op2 = new Longword(); // operand 2
-	private int moveVal;
-	private Longword result = new Longword();
+
+	// Move Instruction
+	private Longword moveVal = new Longword();
+	
+	// Final result
+	private int resultRegister = 0;
+	private Longword result = new Longword(); // result of operation
+	
 
 	public void run() {
 		PC.set(0);
@@ -31,8 +40,10 @@ public class Computer {
 	}
 
 	public void decode() {
-		Longword mask = new Longword();
-		mask.set(15);
+		Longword fourBitMask = new Longword();
+		fourBitMask.set(15); // (tttt)/(1111)
+		Longword eightBitMask = new Longword();
+		eightBitMask.set(255); // (tttttttt)/(11111111)
 
 		// Retrieve the opCode from the current instruction, get nibble 1
 		for (int x = 0; x < 4; x++) {
@@ -42,40 +53,54 @@ public class Computer {
 		// MOVE instruction
 		if (opCode[0].getValue() == false && opCode[1].getValue() == false && opCode[2].getValue() == false
 				&& opCode[3].getValue() == true) {
+
 			// Determine register to move value to (nibble 2)
-			int op1Index = currentInstruction.rightShift(24).leftShift(28).rightShift(28).and(mask).getSigned();
-			op1.copy(registers[op1Index]);
+			resultRegister = currentInstruction.rightShift(24).leftShift(28).rightShift(28).and(fourBitMask).getSigned();
+
+			// Determine the value to move into the register (nibbles 3 and 4)
+			moveVal.copy(currentInstruction.rightShift(16).leftShift(24).rightShift(24).and(eightBitMask));
 			
-			// Determine the value to move into the register
-			
-		} else {
+			// Case for negative value, set remaining bits to true
+			if (moveVal.getBit(24).getValue() == true) {
+				for (int x = 0; x < 24; x++) {
+					moveVal.setBit(x, new Bit(true));
+				}
+			}
+
+		} else { // Otherwise regular decode
+
 			// Determine op1 register, shift to get nibble 2 and then AND with mask of tttt
 			// (1111)
-			int op1Index = currentInstruction.rightShift(24).leftShift(28).rightShift(28).and(mask).getSigned();
+			int op1Index = currentInstruction.rightShift(24).leftShift(28).rightShift(28).and(fourBitMask).getSigned();
 			op1.copy(registers[op1Index]);
 
 			// Determine op2 register, shift to get nibble 3 and then AND with mask of tttt
 			// (1111)
-			int op2Index = currentInstruction.rightShift(20).leftShift(28).rightShift(28).and(mask).getSigned();
+			int op2Index = currentInstruction.rightShift(20).leftShift(28).rightShift(28).and(fourBitMask).getSigned();
 			op2.copy(registers[op2Index]);
-
+			
+			// Determine result register, shift to get nibble 4 and then AND with mask of tttt
+			resultRegister = currentInstruction.rightShift(16).leftShift(28).rightShift(28).and(fourBitMask).getSigned();
 		}
 	}
 
 	public void execute() {
+		// HALT instruction
 		if (opCode[0].getValue() == false && opCode[1].getValue() == false && opCode[2].getValue() == false
 				&& opCode[3].getValue() == false) { // HALT instruction
 			running.setBit(false);
+		}
+		// MOVE instruction
+		else if (opCode[0].getValue() == false && opCode[1].getValue() == false && opCode[2].getValue() == false
+				&& opCode[3].getValue() == true) {
+			result.copy(moveVal);
+			
 		} else { // Otherwise regular op instruction
 			result.copy(ALU.doOp(opCode, op1, op2));
 		}
 	}
 
 	public void store() {
-		Longword mask = new Longword();
-		mask.set(15);
-		// Determine result register, shift to get nibble 4, and then AND with mask
-		int resultIndex = currentInstruction.rightShift(16).leftShift(28).rightShift(28).and(mask).getSigned();
-		registers[resultIndex].copy(result);
+		registers[resultRegister].copy(result);
 	}
 }
