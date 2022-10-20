f39e152dd8e6fad2ec9c28d4b54afa15728edc9b
Nico <Nico@DESKTOP-1TQIHUB.fios-router.home>
2022-10-20 14:56:33 -0400

    Implemented Interrupt, now need to implement initialization/preload

--- ICSI404 Assignments/src/Computer.java
+++ ICSI404 Assignments/src/Computer.java
@@ -15,11 +15,13 @@ public class Computer {
 
 	// Move Instruction
 	private Longword moveVal = new Longword();
-	
+
+	// Interrupt Instruction
+	private Longword interruptParam = new Longword();
+
 	// Final result
 	private int resultRegister = 0;
 	private Longword result = new Longword(); // result of operation
-	
 
 	public void run() {
 		PC.set(0);
@@ -55,20 +57,23 @@ public class Computer {
 				&& opCode[3].getValue() == true) {
 
 			// Determine register to move value to (nibble 2)
-			resultRegister = currentInstruction.rightShift(24).leftShift(28).rightShift(28).and(fourBitMask).getSigned();
+			resultRegister = currentInstruction.rightShift(24).leftShift(28).rightShift(28).and(fourBitMask)
+					.getSigned();
 
 			// Determine the value to move into the register (nibbles 3 and 4)
 			moveVal.copy(currentInstruction.rightShift(16).leftShift(24).rightShift(24).and(eightBitMask));
-			
+
 			// Case for negative value, set remaining bits to true
 			if (moveVal.getBit(24).getValue() == true) {
 				for (int x = 0; x < 24; x++) {
 					moveVal.setBit(x, new Bit(true));
 				}
+
+				// INTERRUPT instruction
+			} else if (opCode[0].getValue() == false && opCode[1].getValue() == false && opCode[2].getValue() == true
+					&& opCode[3].getValue() == false) {
+				interruptParam.copy(currentInstruction.rightShift(16).leftShift(28).rightShift(28).and(fourBitMask));
 			}
-			
-			// INTERRUPT IMPLEMENTATION HERE
-			
 		} else { // Otherwise regular decode
 
 			// Determine op1 register, shift to get nibble 2 and then AND with mask of tttt
@@ -80,29 +85,54 @@ public class Computer {
 			// (1111)
 			int op2Index = currentInstruction.rightShift(20).leftShift(28).rightShift(28).and(fourBitMask).getSigned();
 			op2.copy(registers[op2Index]);
-			
-			// Determine result register, shift to get nibble 4 and then AND with mask of tttt
-			resultRegister = currentInstruction.rightShift(16).leftShift(28).rightShift(28).and(fourBitMask).getSigned();
+
+			// Determine result register, shift to get nibble 4 and then AND with mask of
+			// tttt
+			resultRegister = currentInstruction.rightShift(16).leftShift(28).rightShift(28).and(fourBitMask)
+					.getSigned();
 		}
 	}
 
 	public void execute() {
-		// HALT instruction
+			// HALT instruction
 		if (opCode[0].getValue() == false && opCode[1].getValue() == false && opCode[2].getValue() == false
 				&& opCode[3].getValue() == false) { // HALT instruction
 			running.setBit(false);
 		}
-		// MOVE instruction
+			// MOVE instruction
 		else if (opCode[0].getValue() == false && opCode[1].getValue() == false && opCode[2].getValue() == false
-				&& opCode[3].getValue() == true) {
+				&& opCode[3].getValue() == true) { // MOVE instruction
 			result.copy(moveVal);
-			
-		} else { // Otherwise regular op instruction
+
+			// INTERRUPT instruction	
+		} else if (opCode[0].getValue() == false && opCode[1].getValue() == false && opCode[2].getValue() == true
+				&& opCode[3].getValue() == false) { 
+			if (interruptParam.getBit(31).getValue() == false) { // Print all of the registers
+				for (int x = 0; x < 16; x++) {
+					System.out.println(registers[x].toString());
+				}
+			} else { // Print all 1024 bytes of memory
+				for (int x = 0; x < 1024 ; x++) {
+					Longword address = new Longword();
+					address.set(x);
+					System.out.println(computerMemory.read(address).toString());
+				}
+			}
+			// Otherwise do regular op instruction
+		} else {
 			result.copy(ALU.doOp(opCode, op1, op2));
 		}
 	}
 
 	public void store() {
+		if (opCode[0].getValue() == false && opCode[1].getValue() == false && opCode[2].getValue() == true
+				&& opCode[3].getValue() == false) { // INTERRUPT, nothing happens in store
+			return;
+		}
 		registers[resultRegister].copy(result);
+	}
+	
+	public void preload(String[] preBits) {
+		
 	}
 }
