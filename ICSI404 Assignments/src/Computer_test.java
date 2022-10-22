fe19268d882a32c628f09dacfc78b31863dc9e0a
Nico <Nico@DESKTOP-1TQIHUB.fios-router.home>
2022-10-22 15:30:45 -0400

    Created Computer_test and added previous tests
    Removed comments from Memory_test

---
+++ ICSI404 Assignments/src/Computer_test.java
@@ -0,0 +1,24 @@
+
+public class Computer_test {
+	public static void main (String args[]) {
+		Bit_test.runTests();
+		System.out.println();
+		Longword_test.runTests();
+		System.out.println();
+		RippleAdder_test.runTests();
+		System.out.println();
+		Multiplier_test.runTests();
+		System.out.println();
+		ALU_test.runTests();
+		System.out.println();
+		Memory_test.runTests();
+		runTests();
+	}
+	public static void runTests() {
+		testComputer();
+	}
+	
+	public static void testComputer() {
+		
+	}
+}
